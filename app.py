b7e47d5e00025f598efd7e6f763fb1a4845a1d92
Mickael van Schie <mickael.v.s.19@hotmail.com>
2023-04-13 11:06:52 +0200

    Het werkt nu, wel ietwat wonky

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import json
 from typing import TypedDict
 
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, join_room, send, emit
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['secret_key'] = 'superSecret!'
+app.config['SECRET_KEY'] = 'superSecret!'
 app.debug = True
 socketio = SocketIO(app)
 
@@ -15,16 +14,18 @@ class Client(TypedDict):
     okta_user_id: str
     name: str
     initiator: bool
+    order_number: int
 
 
-order_list: dict[int, [Client]] = {}
 client_list: list[Client] = []
 
 
 @app.route('/', defaults={'name': 'Jan', 'order_number': 100})
 @app.route('/<string:name>', defaults={'order_number': 100})
 @app.route('/<string:name>/<int:order_number>')
-def index(name: str, order_number: int):  # put application's code here
+def index(name: str, order_number: int):  # put application's code here.
+    session['name'] = name
+    session['order_number'] = order_number
     return render_template('index.html', name=name, order_number=order_number)
 
 
@@ -35,48 +36,46 @@ if __name__ == '__main__':
 
 @socketio.on('visit_order')
 def check_order(data):
-    global order_list
     global client_list
-    order = data['order_number']
-    order_being_looked_at = order in order_list
-    client = Client(id=request.sid, okta_user_id="", name=data['user_name'], initiator=False)
-    if not check_user_in_user_list(client['name']):
-        client_list.append(client)
+    order = session['order_number']
+    client = bake_client()
+    initiator = get_order_initiator(order)
+    if initiator:
+        if not check_user_in_user_list(client['name']):
+            client_list.append(client)
 
-    if order_being_looked_at:
-        initiator = get_order_initiator(order)
-        if not check_user_is_in_order(client, order):
-            order_list[order].append(client)
-
-        # Let's send this user a headsup to ask wether they want to take over the current initiator.
-
-        emit('take_over_possible', {"current_client": initiator['name']})
+        if initiator['name'] != client['name']:
+            emit('take_over_possible', {"current_client": initiator['name']})
     else:
-        # Order is not being looked at. Let's create a new entry and set this client as the initiator.
-        # This makes sure that this client is the one which will receive the message that they have
-        # been taken over if required.
         client['initiator'] = True
-        order_list[order] = [client]
+        client_list.append(client)
+    print(client_list)
 
 
 @socketio.on('take_over_request')
 def take_over_request(data):
     global client_list
-    order_number = data['data']
-    order_initiator = get_order_initiator(order_number)
-    client = get_current_client()
-    emit('take_over_request', {"client": client}, room=order_initiator['id'])
+    order_number = session['order_number']
+    initiator = get_order_initiator(order_number)
+    print(initiator, 'initiator')
+    client = get_client(session['name'])
+    emit('take_over_request', {"client": client}, room=initiator['id'])
 
 
 @socketio.on('take_over_accept')
 def take_over_accept(data):
-    global order_list
-    print(data, 'data')
+    global client_list
+    client = data['data']
+    get_client(client['name'])['initiator'] = True
+    initiator = get_order_initiator(session['order_number'])
+    initiator['initiator'] = False
 
+    # Send headsup to overtaker
+    emit('take_over_completed', client, room=get_client(client['name'])['id'])
 
-def check_user_is_in_order(client: Client, room: int) -> bool:
-    global order_list
-    return bool([c for c in order_list[room] if c['name'] == client['name']])
+    # Send headsup to OG initiator
+    print(get_client(session['name']), 'dingetje')
+    emit('take_over_completed', client, room=get_client(session['name'])['id'])
 
 
 def check_user_in_user_list(name: str) -> Client | None:
@@ -86,13 +85,30 @@ def check_user_in_user_list(name: str) -> Client | None:
     return client if client else None
 
 
-def get_order_initiator(order: int) -> Client:
-    global order_list
-    # This should in theory only return one single Client.
-
-    return next(iter([c for c in order_list[order] if c['initiator']]))
-
-
-def get_current_client() -> Client:
+def get_order_initiator(order: int) -> Client | None:
     global client_list
-    return [c for c in client_list if c['id'] == request.sid][0]
+    clients = [c for c in client_list if c['order_number'] == order and c['initiator']]
+    if clients:
+        return clients[0]
+    else:
+        return False
+
+
+def get_client(name: str) -> Client:
+    global client_list
+    return [c for c in client_list if c['name'] == name][0]
+
+
+def bake_client() -> Client:
+    client = [c for c in client_list if c['name'] == session['name']]
+    if client:
+        client[0]['id'] = request.sid
+        return client[0]
+    else:
+        return Client(
+            id=request.sid,
+            okta_user_id='',
+            name=session['name'],
+            initiator=False,
+            order_number=session['order_number']
+        )
